5728d206fbc1ea2fe560e48dc188cb3b78a9e39d
mariuszzielinski22 <info.mariusz.zielinski@gmail.com>
2021-02-06 08:19:15 +0100

    Created def cat

--- oop_sda_1/cat.py
+++ oop_sda_1/cat.py
@@ -2,5 +2,5 @@ class Cat:
     def __init__(self, name: str):
         self.name = name
 
-    def make_sound(self):
+    def make_sound(self) -> str:
         return f'Name is {self.name} sound is meow'
--- oop_sda_1/main.py
+++ oop_sda_1/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello")
+from oop_sda_1.cat import Cat
 
+
+def main():
+    cat_object = Cat("Filemon")
+    sound: str = cat_object.make_sound()
+    print(sound)
 
 if __name__ == "__main__":
     main()
