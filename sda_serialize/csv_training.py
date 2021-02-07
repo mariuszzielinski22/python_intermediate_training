d3d86b0d0e9604de9679ec72c5d51dc2723d2dc9
mariuszzielinski22 <info.mariusz.zielinski@gmail.com>
2021-02-07 21:01:58 +0100

    added read metod, write metod, add data users

---
+++ sda_serialize/csv_training.py
@@ -0,0 +1,41 @@
+import csv
+
+
+def csv_write(users_list):
+    try:
+        with open('./data.csv', 'a') as fd:
+            writer = csv.writer(fd)
+            for user_tuple in users_list:
+                writer.writerow(user_tuple)
+
+
+    except (IOError, Exception) as e:
+        print(f'Exception while writing csv format info: {e.args}')
+
+
+def csv_read():
+    users = []
+    try:
+        with open('./data.csv', 'r') as fd:
+            reader = csv.reader(fd)
+            for row in reader:
+                if row:
+                    users.append(row)
+
+    except (IOError, Exception) as e:
+        print(f'Exception while reading csv format info: {e.args}')
+    return users
+
+
+def main():
+    users = [
+        ('John', 'Smith', 765438),
+        ('Sam', 'Jackson', 689276),
+        ('Mike', 'Jonson', 852789)
+    ]
+    csv_write(users)
+    returned_users = csv_read()
+    print(returned_users)
+
+if __name__ == '__main__':
+    main()
