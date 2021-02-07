a145751300fc6517c9ebd89d9201850852f6f0f1
mariuszzielinski22 <info.mariusz.zielinski@gmail.com>
2021-02-07 11:48:40 +0100

    Add case dictionary

--- exeptions_1/excersises.py
+++ exeptions_1/excersises.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def case_1():
     # given
     list_of_numbers = [1, 5, 8, 10, 21]
@@ -24,3 +27,31 @@ def case_2(name: str):
     if len(name) <= 0:
         raise ValueError('String is empty.')
     print(f"Given name is: {name}")
+
+
+def case_3(number: int, divisor: int) -> float:
+    result = 0
+    try:
+        result = number / divisor
+    except ZeroDivisionError as zde:
+        print(f'Nie dziel przez zero')
+        result = sys.float_info.max
+        # result = float(sys.maxsize)
+    return result
+
+def case_4(dictionary: dict):
+    key = 'items'
+    try:
+        items: list = dictionary[key]
+        for item in items:
+            print(item)
+    except KeyError as key_error:
+        print(f'Key {key} not found, more information{key_error.args}')
+
+def case_4_v_2(dictionary: dict):
+    key = 'items'
+    items: list = dictionary.get(key, [])
+    for item in items:
+        print(item)
+    if not items:
+        print(f'Key does not exist or list is empty')
--- exeptions_1/main.py
+++ exeptions_1/main.py
@@ -1,15 +1,22 @@
-from exeptions_1.excersises import case_1, case_2
+from exeptions_1.excersises import case_1, case_2, case_3, case_4, case_4_v_2
 
 
 def main():
     print('Start up')
 
-    try:
-        case_2('')
-    except ValueError as ve:
-        print(f'{ve.args} returned')
+    # try:
+    #     case_2('')
+    # except ValueError as ve:
+    #     print(f'{ve.args} returned')
 
-    print('Finish')
+    result = case_3(10, 0)
+    print(result)
+    print(f'Finish')
+
+    dictionary = {
+        "rzeczy": ['butter', 'bread']
+    }
+    case_4_v_2(dictionary)
 
 
 if __name__ == '__main__':
