2065e2e6fe5bce35c5da61c0d6fd2081b01b3c0c
mariuszzielinski22 <info.mariusz.zielinski@gmail.com>
2021-02-20 12:19:09 +0100

    Decorators 1

---
+++ decorators/main.py
@@ -0,0 +1,23 @@
+def main():
+    a, b = print_hello_world(1, 2)
+    print(f'In main: {a} {b}')
+
+
+def wrap_before_and_after(func):
+    def wrapper(*args, **kwargs):
+        print('Before')
+        result = func(*args, **kwargs)
+        print('After')
+        return result
+    return wrapper
+
+
+@wrap_before_and_after
+def print_hello_world(a, b):
+    print("Hello world!")
+    print(f'{a, b}')
+    return a, b
+
+
+if __name__ == '__main__':
+    main()
