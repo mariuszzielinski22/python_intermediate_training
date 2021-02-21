8433f069587fd303ee61fede305db6ce11fc44b0
mariuszzielinski22 <info.mariusz.zielinski@gmail.com>
2021-02-21 23:01:07 +0100

    Exercise 4 - randint

--- sda_lambda_1/main.py
+++ sda_lambda_1/main.py
@@ -1,9 +1,11 @@
 from sda_lambda_1.exercise1_3 import exercise_1, exercise_2_3
+from sda_lambda_1.exercise_4 import exercise_4
 
 
 def main():
     exercise_1()
     exercise_2_3()
+    exercise_4()
 
 
 if __name__ == '__main__':
